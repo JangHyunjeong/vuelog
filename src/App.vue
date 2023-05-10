9655153e5a9fe6d370e3b944b5a7d2c858aaac57
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-10 15:47:26 +0900

    list 컴포넌트 분리 및 listData 추가

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,86 @@
 <template>
   <div>
+    <!-- navigation -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">VueLog</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <!-- // navigation -->
+
+    <List :ListData="ListData" />
   </div>
 </template>
 
 <script>
+import List from "./components/List";
+import ListData from "./assets/listData.js";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      ListData: ListData,
+    };
+  },
   components: {
-  }
-}
+    List,
+  },
+};
 </script>
 
 <style>
@@ -19,6 +90,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="(item, idx) in ListData" :key="idx">
+    <h5>{{ item.title }}</h5>
+    <p>{{ item.date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    ListData: Array,
+  },
+};
+</script>
